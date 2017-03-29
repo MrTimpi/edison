8e79c578ba99c3df52da49441c7e23441b7b8867
Oussama Zaki <oussama.zaki@betssongroup.com>
2017-03-29 14:53:35 +0200

    ajax to send registration form

--- public/js/ajax-mail.js
+++ public/js/ajax-mail.js
@@ -1,48 +1,52 @@
-$(function() {
+$(function () {
 
 	// Get the form.
 	var form = $('#contact-form');
-
+	var toObject = function objectifyForm(formArray) {
+		var returnArray = {};
+		for (var i = 0; i < formArray.length; i++) {
+			returnArray[formArray[i]['name']] = formArray[i]['value'];
+		}
+		return returnArray;
+	}
 	// Get the messages div.
 	var formMessages = $('.form-messege');
 
 	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
+	$(form).submit(function (e) {
 		// Stop the browser from submitting the form.
 		e.preventDefault();
 
-		// Serialize the form data.
-		var formData = $(form).serialize();
-		console.log(formData)
+		var formObject = toObject($(form).serializeArray());
 		// Submit the form using AJAX.
 		$.ajax("/api/attendee", {
 			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
+			data: JSON.stringify(formObject),
+  			contentType: 'application/json'
 		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+			.done(function (response) {
+				// Make sure that the formMessages div has the 'success' class.
+				$(formMessages).removeClass('error');
+				$(formMessages).addClass('success');
 
-			// Set the message text.
-			$(formMessages).text(response);
+				// Set the message text.
+				$(formMessages).text(response);
 
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+				// Clear the form.
+				$('#contact-form input.info, #contact-form textarea').val('');
+			})
+			.fail(function (data) {
+				// Make sure that the formMessages div has the 'error' class.
+				$(formMessages).removeClass('success');
+				$(formMessages).addClass('error');
 
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
+				// Set the message text.
+				if (data.responseText !== '') {
+					$(formMessages).text(data.responseText);
+				} else {
+					$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				}
+			});
 	});
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,37 @@
 var express = require('express');
+var expressValidator = require('express-validator');
+var bodyParser = require('body-parser');
 var router = express.Router();
+
+router.use(bodyParser.json());
+router.use(expressValidator());
 
 router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 router.get('/api/attendee', function (req, res) {
-    var user = {
-        name: req.body.name,
-        email: req.body.email
-    };
-    console.log(req);
+    // todo return list of attendee
     return res.json("get ok");
 });
 
 router.post('/api/attendee', function (req, res) {
-    var user = {
-        name: req.body.name,
-        email: req.body.email
-    };
-    console.log(req);
-    return res.json("post ok");
+
+    req.checkBody('email', 'Email is required.').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail(); 
+
+    //todo: check if email already exist
+    //then save it in db
+
+    req.getValidationResult().then(function (result) {
+        try {
+            result.throw();
+            res.send('See you there!');
+        } catch (e) {
+            console.log(e.array());
+            res.status(400).send('oops, we couldn\'t register you!');
+        }
+    });
 });
 
 module.exports = router;
