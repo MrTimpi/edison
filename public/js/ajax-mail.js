713085993c4e05fbc59c7634b78b94cbc0dcecb4
Tim Schönberger <tim.schonberger@gmail.com>
2017-04-28 03:10:07 +0200

    slack invite area

--- public/js/ajax-mail.js
+++ public/js/ajax-mail.js
@@ -2,13 +2,7 @@ $(function () {
 
 	// Get the form.
 	var form = $('#registration-form');
-	var toObject = function objectifyForm(formArray) {
-		var returnArray = {};
-		for (var i = 0; i < formArray.length; i++) {
-			returnArray[formArray[i]['name']] = formArray[i]['value'];
-		}
-		return returnArray;
-	}
+
 	// Get the messages div.
 	var formMessages = $('.form-messege');
 
@@ -48,6 +42,61 @@ $(function () {
 				}
 			});
 	});
+
+	// ---------------------
+	// SLACK
+	// ---------------------
+
+	// Get the form.
+	var slackForm = $('#slack-form');
+
+	// Get the messages div.
+	var slackMessages = $('.slack-messege');
+	// Set up an event listener for the contact form.
+	$(slackForm).submit(function (e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		var formObject = toObject($(slackForm).serializeArray());
+		// Submit the form using AJAX.
+		$.ajax("/api/slack/invite", {
+			type: 'POST',
+			data: JSON.stringify(formObject),
+			contentType: 'application/json'
+		})
+			.done(function (response) {
+				// Make sure that the formMessages div has the 'success' class.
+				$(slackMessages).removeClass('error');
+				$(slackMessages).addClass('success');
+
+				// Set the message text.
+				$(slackMessages).html(response);
+
+				// Clear the form.
+				$('#slack-form input.info, #slack-form textarea').val('');
+			})
+			.fail(function (data) {
+				// Make sure that the formMessages div has the 'error' class.
+				$(slackMessages).removeClass('success');
+				$(slackMessages).addClass('error');
+
+				// Set the message text.
+				if (data.responseText !== '') {
+					$(slackMessages).html(data.responseText);
+				} else {
+					$(slackMessages).html('Oops! An error occured and your message could not be sent.');
+				}
+			});
+	});
+
+	// TO Object
+	var toObject = function objectifyForm(formArray) {
+		var returnArray = {};
+		for (var i = 0; i < formArray.length; i++) {
+			returnArray[formArray[i]['name']] = formArray[i]['value'];
+		}
+		return returnArray;
+	}
 
 	// --------------
 	// participant count
